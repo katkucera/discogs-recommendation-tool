f1b1f057f125656752e04232c14934efad8f80d0
Kat Kucera <kucerakat@gmail.com>
2019-10-11 20:54:53 -0400

    In discogser.py: added logic to write release uris to user_list_date.txt.

--- discogser.py
+++ discogser.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 
-d = discogs_client.Client('discgos_search_tool')
+d = discogs_client.Client('disocgs_search_tool')
 
 
 def parse_url_to_release(url):
@@ -12,27 +12,28 @@ def parse_url_to_release(url):
 
     return d.master(id) if is_master_release(url) else d.release(id).master
 
+
 def get_related_release_ids(start_releases):
     related_releases = []
 
     # for each release get array of list ids
-    
+
     for release in start_releases:
 
         user_lists = get_user_list_parents(release)
-        
+
         related_releases += [get_sibling_releases(list_id) for list_id in user_lists]
 
     return related_releases
-    
+
 
 # done!
 def get_user_list_parents(release):
     user_list_ids = []
     list_divs = []
-    
+
     source = requests.get(release.url).text
-    
+
     page_soup = soup(source, 'html.parser')
 
     lists_container = page_soup.find('div',{'id':'lists'})
@@ -49,20 +50,29 @@ def get_user_list_parents(release):
 def get_sibling_releases(user_list_id):
     sibling_releases = []
     print(user_list_id)
-    
+
     user_list_url = 'https://api.discogs.com/lists/{}'.format(user_list_id)
     try:
         user_list_data = requests.get(user_list_url).json()
         print(user_list_data['name'])
+        file1 = open("user_list_data.txt", "w+")
+        for release in user_list_data['items']:
+            try:
+                file1.write(str(release['uri'])+"\n")
+            except KeyError:
+                try:
+                    file1.write(str(release['display_title'])+'\n')
+                except KeyError:
+                    file1.write('One release skipped\n')
+
     except ValueError:
         print('Whelp, you found Chris\' Eternity Tank!¯\_(ツ)_/¯')
-    
+
    # print(user_list_data)
    # sibling_releases += requests.get(user_list_url)
-    
+
     return []
 
 
 def is_master_release(url):
     return True if 'master' in url else False
-    
